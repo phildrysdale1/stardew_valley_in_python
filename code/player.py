20f00aea7cd312d2a1675c9dcb7c5b1d636a3d07
Phil Drysdale <100496729+phildrysdale1@users.noreply.github.com>
2022-09-24 22:37:27 +0100

    ++ Player Idle / Tool Selection
    ++ Animation for idling
    ++ Added Timer.py to handle game timers
    ++ Animate selected tools
    
    ~~ Still need to add changing between tools

--- code/player.py
+++ code/player.py
@@ -2,6 +2,7 @@
 import pygame
 from settings import *
 from support import *
+from timer import Timer
 
 ## ========== Classes ========== ##
 
@@ -10,7 +11,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
 
         self.import_assets()
-        self.status = 'down_idle'
+        self.status = 'down'
         self.frame_index = 0
 
         # general setup
@@ -22,6 +23,17 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+        # timers
+        self.timers = {
+            'tool use' : Timer(350, self.use_tool)
+            }
+        
+        # tools
+        self.selected_tool = 'axe'
+
+    def use_tool(self):
+        print(self.selected_tool)
+
     def import_assets(self):
         self.animations = {'up' : [], 'down' : [], 'left' : [], 'right' : [],
                             'right_idle' : [], 'left_idle' : [], 'up_idle' : [], 'down_idle' : [],
@@ -32,6 +44,54 @@ class Player(pygame.sprite.Sprite):
             full_path = 'stardew_valley_in_python/graphics/character/' + animation
             self.animations[animation] = import_folder(full_path)
         print(self.animations)
+
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+        self.image = self.animations[self.status][int(self.frame_index)]
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if not self.timers['tool use'].active:
+            # movement
+            if keys[pygame.K_w]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_s]:
+                self.direction.y = 1
+                self.status = 'down'
+            else:
+                self.direction.y = 0
+            if keys[pygame.K_a]:
+                self.direction.x = -1
+                self.status = 'left'
+            elif keys[pygame.K_d]:
+                self.direction.x = 1
+                self.status = 'right'
+            else:
+                self.direction.x = 0
+
+            # tool use
+            if keys[pygame.K_SPACE]:
+                # timer for the tool use
+                self.timers['tool use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.frame_index = 0
+
+
+    def get_status(self):
+        # idle animation
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split('_')[0] + '_idle'
+        
+        # tool use animation
+        if self.timers['tool use'].active:
+            self.status = self.status.split('_')[0] + '_' + self.selected_tool
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
 
     def move(self, dt):
         # normalising the vector
@@ -46,22 +106,10 @@ class Player(pygame.sprite.Sprite):
         self.pos.y += self.direction.y * self.speed * dt
         self.rect.centery = self.pos.y
 
-    def input(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_w]:
-            self.direction.y = -1
-        elif keys[pygame.K_s]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
-        if keys[pygame.K_a]:
-            self.direction.x = -1
-        elif keys[pygame.K_d]:
-            self.direction.x = 1
-        else:
-            self.direction.x = 0   
             
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
+        self.update_timers()
