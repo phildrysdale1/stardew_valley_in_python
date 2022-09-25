332a2cc3c1132968e1ec3c4b216eadc195eb303d
Phil Drysdale <100496729+phildrysdale1@users.noreply.github.com>
2022-09-25 17:36:31 +0100

    ++ Camera & Flooring
    ++ Customized drawing of sprites
    ++ Camera group for all sprites
    ++ Camera to follow player around map
    ++ Sprites.py file for managing sprites
    ++ Layering sprites with Z position for fake 3D effect

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ class Game:
 	def __init__(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-		pygame.display.set_caption('Sprout land')
+		pygame.display.set_caption('Tilly\'s Farm')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
 	## ========== Main game loop ========== ##
